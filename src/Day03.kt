4a21b4dbc356fc9827cbb7c3a28278e645746094
AemsonM <aemson.michael@yara.com>
2022-12-13 14:30:43 +0100

    feat: added day 03 part 1 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,3 +1,18 @@
 fun main() {
     val inputData = readInput("Day03_input")
+    val commonItems = inputData.map { item ->
+        val numberOfItems = item.toCharArray().size
+        val firstSet = item.substring(0, numberOfItems / 2).toCharArray().toList()
+        val secondSet = item.substring(numberOfItems / 2, numberOfItems).toCharArray().toList()
+        firstSet.intersect(secondSet.toSet()).first()
+
+    }
+    println("Rucksack Reorganization (Part 1) -> ${commonItems.map { it.priority() }.sumOf { it }}")
 }
+
+private fun Char.priority(): Int =
+    when (this) {
+        in 'a'..'z' -> (this - 'a') + 1
+        in 'A'..'Z' -> (this - 'A') + 27
+        else -> throw IllegalArgumentException("Letter not in range: $this")
+    }
