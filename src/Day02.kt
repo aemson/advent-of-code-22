78c247e73b41b297149f5cedda154a4c00573841
AemsonM <aemson.michael@yara.com>
2022-12-13 13:37:38 +0100

    feat: solved day 2 puzzle part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -3,28 +3,38 @@ import GameChoices.PAPER
 import GameChoices.SCISSORS
 
 fun main() {
-    val inputData = readInput("Day02_input_sample")
+    val inputData = readInput("Day02_input")
     val rock =
         GameOption(
             choice = ROCK,
             choicePoint = 1,
             firstPlayerChoice = 'A',
             secondPlayerChoice = 'X',
-            winOver = SCISSORS
+            winOver = SCISSORS,
+            loseOver = PAPER
         )
     val paper =
-        GameOption(choice = PAPER, choicePoint = 2, firstPlayerChoice = 'B', secondPlayerChoice = 'Y', winOver = ROCK)
+        GameOption(
+            choice = PAPER,
+            choicePoint = 2,
+            firstPlayerChoice = 'B',
+            secondPlayerChoice = 'Y',
+            winOver = ROCK,
+            loseOver = SCISSORS
+        )
     val scissors = GameOption(
         choice = SCISSORS,
         choicePoint = 3,
         firstPlayerChoice = 'C',
         secondPlayerChoice = 'Z',
-        winOver = PAPER
+        winOver = PAPER,
+        loseOver = ROCK
     )
     val gameRules = listOf(rock, paper, scissors)
 
     val pointsMap = mapOf("win" to 6, "lost" to 0, "draw" to 3)
     partOneGamePoints(inputData, gameRules, pointsMap)
+    partTwoGamePoints(inputData, gameRules, pointsMap)
 }
 
 private fun partOneGamePoints(
@@ -39,16 +49,47 @@ private fun partOneGamePoints(
 
         val firstPlayer = gameRules.find { it.firstPlayerChoice == firstPlayerChoice }
         val secondPlayer = gameRules.find { it.secondPlayerChoice == secondPlayerChoice }
-        if (secondPlayer?.winOver == firstPlayer?.choice) {
-            secondPlayer?.choicePoint?.plus(pointsMap["win"] ?: 6)
-        } else if (firstPlayer?.winOver == secondPlayer?.choice) {
-            secondPlayer?.choicePoint?.plus(pointsMap["lost"] ?: 0)
-        } else {
-            secondPlayer?.choicePoint?.plus(pointsMap["draw"] ?: 3)
-        }
+        calculatePoints(secondPlayer, firstPlayer, pointsMap)
     }
     println("Part 1 -> Game Point Total: ${gamePoints.sumOf { it!! }}")
 }
+
+/**
+ * X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+ * */
+fun partTwoGamePoints(
+    inputData: List<String>,
+    gameRules: List<GameOption>,
+    pointsMap: Map<String, Int>
+) {
+    val newRule = mapOf('X' to "lost", 'Y' to "draw", 'Z' to "win")
+    val gamePoints = inputData.map { game ->
+        val playerChoices = game.split(" ")
+        val firstPlayerChoice = playerChoices[0].single()
+        val secondPlayerChoice = playerChoices[1].single()
+        val firstPlayer = gameRules.find { it.firstPlayerChoice == firstPlayerChoice }
+        val secondPlayer = when (secondPlayerChoice) {
+            'X' -> gameRules.first { it.choice == firstPlayer?.winOver }
+            'Y' -> gameRules.first { it.choice == firstPlayer?.choice }
+            else -> gameRules.first { it.choice == firstPlayer?.loseOver }
+        }
+        calculatePoints(secondPlayer, firstPlayer, pointsMap)
+    }
+    println("Part 2 -> Game Point Total: ${gamePoints.sumOf { it!! }}")
+}
+
+private fun calculatePoints(
+    secondPlayer: GameOption?,
+    firstPlayer: GameOption?,
+    pointsMap: Map<String, Int>
+) = if (secondPlayer?.winOver == firstPlayer?.choice) {
+    secondPlayer?.choicePoint?.plus(pointsMap["win"] ?: 6)
+} else if (firstPlayer?.winOver == secondPlayer?.choice) {
+    secondPlayer?.choicePoint?.plus(pointsMap["lost"] ?: 0)
+} else {
+    secondPlayer?.choicePoint?.plus(pointsMap["draw"] ?: 3)
+}
+
 
 enum class GameChoices {
     ROCK,
@@ -57,9 +98,10 @@ enum class GameChoices {
 }
 
 data class GameOption(
-    val choice: GameChoices,
+    var choice: GameChoices,
     val choicePoint: Int,
     val firstPlayerChoice: Char,
     val secondPlayerChoice: Char,
-    val winOver: GameChoices
+    var winOver: GameChoices,
+    var loseOver: GameChoices,
 )
